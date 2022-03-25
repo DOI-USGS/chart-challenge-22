8a9ef32fee1d5a290ecdde21bfa0cedea7a8f78a
Jordan S. Read <jread@usgs.gov>
2022-03-25 15:53:20 -0500

    with vectors!

--- 03_historical_jread/src/data_utils.R
+++ 03_historical_jread/src/data_utils.R
@@ -12,44 +12,48 @@ summarize_nc_time <- function(year0, year1, mm_dd0, mm_dd1, ...){
     seq(as.Date(sprintf('%s-%s', yy, mm_dd0)), to = as.Date(sprintf('%s-%s', yy, mm_dd1)), by = 'days')
   }) %>% unlist() %>% as_date()
 
-  GDD_doy <- function(x, group_values, gdd_perc = 0.5){
+  # for sampling
+  set.seed(1)
 
-    tibble(x = x, groups = group_values) %>% group_by(groups) %>%
-      summarize(idx = which.min(abs(cumsum(x) - sum(x)*gdd_perc))) %>%
-      pull(idx)
-
-  }
   summarized_data <- purrr::map(data_files, function(pred_fl){
     nc <- nc_open(pred_fl)
     time_origin <- ncdf4::ncatt_get(nc, 'time', attname = 'units')$value %>% str_remove("days since ")
     time <- ncvar_get(nc, 'time') + as.Date(time_origin)
-    time_idx <- time %in% use_times
+    time_idx <- which(time %in% use_times)
+
     site_ids <- ncvar_get(nc, 'site_id')
+
+    lake_lat <-  ncvar_get(nc, 'lat')
+    lake_lon <-  ncvar_get(nc, 'lon')
+
+    use_idx <- sample(1:length(lake_lat), size = 10000)
 
     # create a year group, and calculate information for each
     # verify use_times is identical order to actual times!!
     yyyy_groups <- lubridate::year(use_times)
-    all_surftemp <- ncvar_get(nc, 'surftemp', start = c(1, 1), count = c(-1, -1)) %>%
-      {.[time_idx, ]} %>% as_tibble(.name_repair = 'unique') %>%
+
+    data_out <- ncvar_get(nc, 'surftemp', start = c(first(time_idx), 1),
+                          count = c(last(time_idx) - first(time_idx) + 1, -1)) %>%
+      {.[, use_idx]} %>% as_tibble(.name_repair = 'unique') %>%
+      # would it be better to pivot longer here?
       mutate(group_values = yyyy_groups) %>%
       group_by(group_values) %>%
       # find index of day when it is the percentage of GDD
       # need to make this GDD instead of just a rolling sum:
-      summarize_all(.funs = function(x) which.min(abs(cumsum(x) - sum(x)*0.5))) %>%
+      summarize_all(.funs = list(GDD_time = ~ which.min(abs(cumsum(.) - sum(.)*0.5)),
+                                 GDD_mean = ~ sum(.))) %>%
       select(-group_values) %>%
       ungroup() %>%
       # take the mean of all years in the collection
       summarize_all(.funs = mean) %>%
-      # flatten this out to a vector
-      t() %>% as.vector()
+      pivot_longer(everything(), names_pattern =  '(.*)(GDD_time|GDD_mean)', names_to = c('site_id','stat')) %>%
+      pivot_wider(names_from = stat, values_from = value) %>%
+      mutate(lat = lake_lat[use_idx], lon = lake_lon[use_idx]) %>%
+      select(-site_id)
 
 
-
-    browser()
-    lake_lat <-  ncvar_get(nc, 'lat')
-    lake_lon <-  ncvar_get(nc, 'lon')
     nc_close(nc)
-    tibble(surftemp = all_surftemp, lat = lake_lat, lon = lake_lon)
+    data_out
   }) %>% bind_rows() %>%
     st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
     st_transform("+proj=lcc +lat_1=30.7 +lat_2=29.3 +lat_0=28.5 +lon_0=-91.33333333333333 +x_0=999999.9999898402 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs")
--- 03_historical_jread/src/plot_utils.R
+++ 03_historical_jread/src/plot_utils.R
@@ -1,12 +1,26 @@
 plot_temp_diff <- function(fileout, early_data, late_data){
-  del_data <- late_data %>% rename(late_temp = surftemp) %>%
-    st_join(early_data) %>% mutate(temp_diff = late_temp - surftemp)
+
+
+
+  del_data <- late_data %>% rename(late_GDD_time = GDD_time, late_GDD_mean = GDD_mean) %>%
+    st_join(early_data) %>%
+    mutate(GDD_timing_diff = late_GDD_time - GDD_time,
+           GDD_mean_diff = late_GDD_mean - GDD_mean) %>%
+    mutate(x = st_coordinates(.)[,1], y = st_coordinates(.)[,2],
+           x_dif = GDD_timing_diff*10000,
+           y_dif = GDD_mean_diff*200,
+           xend = x - x_dif,
+           yend = y + y_dif,
+           angle = (atan2(y_dif,x_dif)*180)/pi) %>%
+    st_drop_geometry()
 
   ggplot() +
-    geom_sf(data = del_data, size = 0.5, aes(col=temp_diff)) +
-    scale_colour_viridis_c(option = "magma")
+    coord_sf(crs = st_crs(early_data)) +
+    geom_segment(del_data, mapping = aes(col = angle, x = x, y = y, xend = xend, yend = yend),
+                 size = 0.2) +
+    scale_colour_viridis_c(option = "magma", direction = -1)
 
-  ggsave(filename = fileout)
+  ggsave(filename = fileout, width = 16, height = 10)
 
 
 }
