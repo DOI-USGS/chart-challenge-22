17ee943c4a5092304f6f3c45df30ff248ac1178c
Jordan S. Read <jread@usgs.gov>
2022-03-26 09:04:05 -0500

    style updates

--- 03_historical_jread/src/plot_utils.R
+++ 03_historical_jread/src/plot_utils.R
@@ -1,13 +1,13 @@
 plot_temp_diff <- function(fileout, early_data, late_data){
 
   # CRS-specific scaling for vectors and cells.
-  cell_size <- 60000
-  y_scale <- 250
+  cell_size <- 60000 # smaller number for finer resolution = more busy
+  y_scale <- 300 # smaller number for shorter vectors
 
-  # scale agnostic params
-  x_y_ratio <- 75
+  # scale agnostic params:
+  x_y_ratio <- 75 # relationship between x and y components, since they are diff units
   x_scale <- x_y_ratio * y_scale
-  lwd <- 0.3
+  lwd <- 0.35 # line width for vectors
 
   # going to resample this to a hex grid so it isn't too busy:
   site_grid <- st_make_grid(early_data, cellsize = cell_size, square = FALSE, offset = c(-125, 25)) %>%
@@ -16,40 +16,33 @@ plot_temp_diff <- function(fileout, early_data, late_data){
     mutate(cell_id = row_number()) %>%
     st_transform(st_crs(early_data))
 
-  # make a vector that is straight up and large:
-  late_data[3000,]$GDD_mean <- late_data[3000,]$GDD_mean + 100000
-  late_data[3000,]$GDD_doy_mean <- early_data[3000,]$GDD_doy_mean
-
-  # make a vector that is to the right and large:
-  late_data[2,]$GDD_mean <- early_data[2,]$GDD_mean
-  late_data[2,]$GDD_doy_mean <- late_data[2,]$GDD_doy_mean + 500
-
-  # make a vector that is to the left and large:
-  late_data[6000,]$GDD_mean <- early_data[6000,]$GDD_mean
-  late_data[6000,]$GDD_doy_mean <- late_data[6000,]$GDD_doy_mean - 500
-
   del_data <- late_data %>% rename(late_GDD_doy = GDD_doy_mean, late_GDD_mean = GDD_mean) %>%
     st_join(early_data) %>%
+    # calculate the difference in timing and ave yearly GDD between the periods
     mutate(GDD_timing_diff = late_GDD_doy - GDD_doy_mean,
            GDD_mean_diff = late_GDD_mean - GDD_mean) %>%
+    # calculate vectors for each lake
     mutate(x = st_coordinates(.)[,1], y = st_coordinates(.)[,2],
            x_dif = GDD_timing_diff * x_scale,
            y_dif = GDD_mean_diff*y_scale,
-           xend = x - x_dif,
+           xend = x + x_dif,
            yend = y + y_dif) %>%
+    # group to cells, use averages within each cell to make it less busy
     st_intersection(site_grid, .) %>%
     group_by(cell_id) %>%
     st_drop_geometry() %>%
     summarize_all(mean) %>%
+    # calc angle of cell-based vector, which is an ave of all lakes in cell:
     mutate(angle = (atan2(y_dif,x_dif)*180)/pi)
 
+  # access Contiguous U.S. state map:
   US_states <- sf::st_transform(spData::us_states, st_crs(early_data))
 
   ggplot() +
     geom_sf(data = US_states, color = 'grey90', fill = 'white', size = 0.75) +
     geom_segment(del_data, mapping = aes(col = angle, x = x, y = y, xend = xend, yend = yend),
                  size = lwd) +
-    scico::scale_color_scico(palette = "romaO", direction = -1)
+    scico::scale_color_scico(palette = "romaO", direction = -1, midpoint = 0)
 
   ggsave(filename = fileout, width = 16, height = 10)
 
