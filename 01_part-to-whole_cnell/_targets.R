c8cf1d41e5f68664d71ba967a13234fc6b18d16e
collnell <cnell@usgs.gov>
2022-03-31 11:57:34 -0500

    move data to out directory

--- 01_part-to-whole_cnell/_targets.R
+++ 01_part-to-whole_cnell/_targets.R
@@ -13,7 +13,7 @@ proj_aea <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_
 list(
   tar_target(
     state_file, 
-    scrape_table('in/state_water_area.csv', wss_url),
+    scrape_table('out/state_water_area.csv', wss_url),
     format = "file"
   ),
   tar_target(
