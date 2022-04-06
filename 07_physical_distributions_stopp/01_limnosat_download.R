6cb8d56b59fd9bfd76ebc6af4a5c70c8a7ca5ea2
Platt <lplatt@usgs.gov>
2022-04-06 13:51:02 -0500

    was able to use `curl` to download without any issues and without needing to change the timeout

--- 07_physical_distributions_stopp/01_limnosat_download.R
+++ 07_physical_distributions_stopp/01_limnosat_download.R
@@ -4,6 +4,7 @@ library(sf)
 library(feather)
 library(lubridate)
 library(leafgl)
+library(curl)
 
 setwd('07_physical_distributions_stopp/')
 ## First we need to download the actual database, if you've already downloaded it, skip this first chunk.
@@ -24,24 +25,20 @@ if (file.exists(folder)){
   folder <- paste0(folder,'/')
 }
 
-# Incerase out timeout because some of these are large files
-options(timeout = max(6000, getOption("timeout")))
-
-
 ##Download the Deepest point shapefile.  This contains the locations of all the lakes in the database.
 ## Note: On windows you need mode = 'wb' over the default mode = 'w' for download.file)
-grep('DP', urls, value = T) %>% purrr::map(., ~download.file(., paste0(folder,basename(.)), mode = 'wb'))
+grep('DP', urls, value = T) %>% purrr::map(., ~curl_download(., paste0(folder,basename(.)), mode = 'wb'))
 
 ## Download the scene metadata.  This includes things like scene cloud cover and sun angle for all the
 ## remote sensing observations in LimnoSat-US.
 meta.url <- grep('SceneMetadata', urls, value = T)
-download.file(meta.url, paste0(folder,basename(meta.url)), mode = 'wb')
+curl_download(meta.url, paste0(folder,basename(meta.url)), mode = 'wb')
 
 ## Download the actual LimnoSat Database, here we'll download the .feather version because it's
 ## a little smaller, if you'd prefer the csv, just swap out the .feather with .csv below
 ## Note: This takes  a couple minutes because the file is ~3gb. Also, we'll rename the file to be
 ## more user friendly.
 ls.url <- grep('srCorrected_us_hydrolakes_dp_20200628.csv', urls, value = T)
-download.file(ls.url, paste0(folder, 'LimnoSat_20200628.csv'), mode = 'wb')
+curl_download(ls.url, paste0(folder, 'LimnoSat_20200628.csv'), mode = 'wb')
 
 rm(ls.url, ls.urls, meta.url, urls, files)
