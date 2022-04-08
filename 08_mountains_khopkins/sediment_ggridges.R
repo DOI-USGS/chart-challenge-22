906e431311d935f1b57e8cc2b135775c6a16b1d4
collnell <cnell@usgs.gov>
2022-04-07 22:39:41 -0500

    reduce duplicate df names

--- 08_mountains_khopkins/sediment_ggridges.R
+++ 08_mountains_khopkins/sediment_ggridges.R
@@ -19,69 +19,66 @@ showtext_auto()
 # Download data from https://www.sciencebase.gov/catalog/item/5bb4de01e4b08583a5da4477
 # and put in "in/" folder
 
-# Read in data
-data_df = read.csv("in/SPARROWmodeldat/data1_vTSS/data1_vTSS.csv", stringsAsFactors = FALSE)
-predict_df = read.csv("in/SPARROWmodeldat/predict_TSS/predict_TSS.csv", stringsAsFactors = FALSE)
+# Read in sediment data
+DF_data = read.csv("in/SPARROWmodeldat/data1_vTSS/data1_vTSS.csv", stringsAsFactors = FALSE)
+DF_predict = read.csv("in/SPARROWmodeldat/predict_TSS/predict_TSS.csv", stringsAsFactors = FALSE)
 
-# Join
-DF = left_join(data_df, predict_df, by = "comid")
-rm(data_df, predict_df)
-colnames(DF)
+# HUC12s
+DF_HUC = read.csv("in/Catchment_HUC12.csv", stringsAsFactors = FALSE)
 
-# Add HUC12s
-DF3 = read.csv("in/Catchment_HUC12.csv", stringsAsFactors = FALSE)
-DF = left_join(DF, DF3, by = "comid")
+# Join data and rename ecoregions
+DF_sediment = left_join(DF_data, DF_predict, by = "comid") %>%
+  left_join(DF_HUC) %>% 
+  drop_na(EcoRegion) %>% 
+  filter(EcoRegion != 0) %>% 
+  mutate(EcoRegion = case_when(
+    EcoRegion %in% c(1,2) ~ "Coastal Plain",
+    EcoRegion %in% c(3,4) ~ "Piedmont",
+    EcoRegion %in% c(5,6) ~ "Mountains",
+    TRUE ~ as.character(EcoRegion) 
+  )) %>%
+  transform(EcoRegion = factor(EcoRegion, levels = c("Mountains", "Piedmont", "Coastal Plain")))
+str(DF_sediment)
 
-unique(DF3$HUC_8)
-
-# Rename Ecoregions
-DF = DF %>% drop_na(EcoRegion)
-DF = DF %>% filter(EcoRegion != 0)
-unique(DF$EcoRegion)
-
-# Rename and combine
-DF$EcoRegion[DF$EcoRegion == 1] <- "Coastal Plain" #Middle Atlantic Coastal Plain
-DF$EcoRegion[DF$EcoRegion == 2] <- "Coastal Plain" #Southeastern Plains
-DF$EcoRegion[DF$EcoRegion == 3] <- "Piedmont" #Northern Outer Piedmont
-DF$EcoRegion[DF$EcoRegion == 4] <- "Piedmont" #Piedmont
-DF$EcoRegion[DF$EcoRegion == 5] <- "Mountains" #Blue Ridge
-DF$EcoRegion[DF$EcoRegion == 6] <- "Mountains" #Ridge and Valley
-
-DF$EcoRegion = factor(DF$EcoRegion, levels = c("Mountains", "Piedmont", "Coastal Plain"))
-
-unique(DF$EcoRegion)
+# Clean up env
+rm(DF_data, DF_predict, DF_HUC)
 
 # Calculate incremental yield and combine sources
-DF_summary = DF %>% group_by(HUC_8) %>% summarize(PLOAD_INC_TOTAL_mg.km2 = sum(PLOAD_INC_TOTAL)/sum(IncAreaKm2),
-                   LandUseChange = sum(PLOAD_INC_NWALTCHG)/sum(IncAreaKm2),
-                   Agriculture = sum(PLOAD_INC_CROPNWALT_KM2)/sum(IncAreaKm2),
-                   Development = sum(PLOAD_INC_DEVNWALT_KM2)/sum(IncAreaKm2),
-                   StreamChannel = sum(PLOAD_INC_SAI_MEAN)/sum(IncAreaKm2) + sum(PLOAD_INC_OPEN)/sum(IncAreaKm2))
-
-DF_Sub = DF_summary[,c(1,3:6)]
-DF_gather = gather(DF_Sub, key = "Source", value = "value", -HUC_8)
+DF_summary = sediment_df %>% 
+  group_by(HUC_8) %>% 
+  summarize(PLOAD_INC_TOTAL_mg.km2 = sum(PLOAD_INC_TOTAL)/sum(IncAreaKm2),
+            LandUseChange = sum(PLOAD_INC_NWALTCHG)/sum(IncAreaKm2),
+            Agriculture = sum(PLOAD_INC_CROPNWALT_KM2)/sum(IncAreaKm2),
+            Development = sum(PLOAD_INC_DEVNWALT_KM2)/sum(IncAreaKm2),
+            StreamChannel = sum(PLOAD_INC_SAI_MEAN)/sum(IncAreaKm2) + sum(PLOAD_INC_OPEN)/sum(IncAreaKm2))
 
 # Find ecoregion with the largest area in each HUC8
-# Add ecoregion to DF_gather
-DF_Eco = DF %>% 
+# Add ecoregion to DF_long
+DF_Eco = DF_sediment %>% 
   group_by(HUC_8, EcoRegion) %>% 
-  summarize(area = sum(IncAreaKm2))
-DF_Eco_Top = DF_Eco %>% group_by(HUC_8) %>% top_n(1, area) # Find largest area
-DF_Eco_Top = DF_Eco_Top %>% drop_na(HUC_8)
+  summarize(area = sum(IncAreaKm2)) %>% 
+  group_by(HUC_8) %>% 
+  top_n(1, area) %>% # Find largest area 
+  drop_na(HUC_8)
 
-DF_gather = left_join(DF_gather, DF_Eco_Top, by = "HUC_8")
-DF_gather = DF_gather %>% drop_na(EcoRegion)
+# Make long DF with different sediment sources
+DF_long = DF_summary %>%
+  select(HUC_8, LandUseChange, Agriculture, Development, StreamChannel) %>%
+  gather(key = "Source", value = "value", -HUC_8) %>%
+  left_join(DF_Eco_Top, by = "HUC_8") %>% 
+  drop_na(EcoRegion)
 
-# Find largest source per EcoRegion
-DF_gather_sediment = DF_gather %>% 
+# Find largest sediment source per EcoRegion
+DF_gather_sediment = DF_long %>% 
   group_by(EcoRegion, Source) %>% 
   summarize(Total_Sediment = sum(value))
 
-DF_gather_sediment_top = DF_gather_sediment %>% group_by(EcoRegion) %>% top_n(2, Total_Sediment) # Find largest area
-
+DF_gather_sediment_top = DF_gather_sediment %>% 
+  group_by(EcoRegion) %>% 
+  top_n(2, Total_Sediment) # Find largest area
 
 # Plot
-ggplot(DF_gather, aes(x = value, y = as.factor(EcoRegion), fill = Source)) +
+ggplot(DF_long, aes(x = value, y = as.factor(EcoRegion), fill = Source)) +
   geom_density_ridges_gradient(scale = 2, rel_min_height = 0.01, gradient_lwd = 2, show.legend = TRUE) +
   scale_x_continuous(expand = c(0.01, 0)) +
   scale_y_discrete(expand = c(0.01, 0)) +
@@ -95,11 +92,8 @@ ggplot(DF_gather, aes(x = value, y = as.factor(EcoRegion), fill = Source)) +
         legend.text=element_text(size=16),
         legend.position= c(0.7,0.8))
 
-colnames(DF)
-
 # Save
-# This isnt working well.
-ggsave("out/Sparrow_chart.tiff", 
+ggsave("out/Sparrow_chart.png", 
        plot = last_plot(),
-       width = 6, height = 4, units = c("in"),
+       width = 12, height = 8, units = c("in"),
        dpi = 300)
