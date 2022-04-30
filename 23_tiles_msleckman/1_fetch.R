f91c0ae90d00bb0867425912e4a8f8080e47f1e8
collnell <cnell@usgs.gov>
2022-04-30 12:40:01 -0500

    group data fetch targets in script

--- 23_tiles_msleckman/1_fetch.R
+++ 23_tiles_msleckman/1_fetch.R
@@ -3,6 +3,30 @@ source('1_fetch/src/fetch_nlcd.R')
 
 p1_targets_list <- list(
   
+  ## Get drb boundary shp, unzip and aggregate to region
+  ## https://www.sciencebase.gov/catalog/item/5d94949de4b0c4f70d0db64f
+  tar_target(
+    p1_drb_boundary_zip,
+    sbtools::item_file_download(sb_id = '5d94949de4b0c4f70d0db64f',
+                                names = 'physiographic_regions_DRB.zip',
+                                destinations = '1_fetch/out/physiographic_regions_DRB.zip',
+                                overwrite_file = TRUE),
+    format = 'file'
+  ),
+  tar_target(
+    p1_drb_boundary_unzip,
+    unzip(p1_drb_boundary_zip, exdir = '1_fetch/out/physiographic_regions_DRB', overwrite = TRUE),
+    format = 'file'
+  ),
+  tar_target(
+    p1_drb_boundary,
+    st_read(p1_drb_boundary_unzip %>% str_subset('.shp$')) %>%
+      group_by(Source) %>%
+      summarize() %>% 
+      mutate(region = 'drb') 
+    ## keep the projection of this file
+  ), 
+  
   ## Download all historical FORESCE data available for DRB for 1640  - 2010
   ## https://www.sciencebase.gov/catalog/item/605c987fd34ec5fa65eb6a74
   tar_target(
@@ -25,47 +49,7 @@ p1_targets_list <- list(
       pattern = '1900|1910|1920|1930|1940|1950|1960|1970|1980|1990')
     ),
   
-  ## Get drb boundary shp, unzip and aggregate to region
-  ## https://www.sciencebase.gov/catalog/item/5d94949de4b0c4f70d0db64f
-  tar_target(
-    p1_drb_boundary_zip,
-    sbtools::item_file_download(sb_id = '5d94949de4b0c4f70d0db64f',
-                                names = 'physiographic_regions_DRB.zip',
-                                destinations = '1_fetch/out/physiographic_regions_DRB.zip',
-                                overwrite_file = TRUE),
-    format = 'file'
-    ),
-  tar_target(
-    p1_drb_boundary_unzip,
-    unzip(p1_drb_boundary_zip, exdir = '1_fetch/out/physiographic_regions_DRB', overwrite = TRUE),
-    format = 'file'
-  ),
-  tar_target(
-    p1_drb_boundary,
-    st_read(p1_drb_boundary_unzip %>% str_subset('.shp$')) %>%
-      group_by(Source) %>%
-      summarize() %>% 
-      mutate(region = 'drb') 
-      ## keep the projection of this file
-      ), 
-  
-  # use DRB boundary to get flowlines from NHD
-  # NHD has streamorder for mapping stream width to later on
-  tar_target(
-    p1_drb_flines,
-    get_nhdplus(AOI = p1_drb_boundary, realization = 'flowline')
-  ),
-
-  tar_target(
-    p1_streams_polylines_drb,
-    p1_drb_flines %>%
-      group_by(streamorde) %>%
-      summarize() %>%
-      rmapshaper::ms_simplify() %>%
-      st_intersection(p1_drb_boundary)
-  ),
-  
-  ## Get nlcd data using the FedData package and the get_nlcd() function 
+  ## Get more recent nlcd data using the FedData package and the get_nlcd() function 
   ## This process includes fetching + masking raster to aoi (drb aoi in our case) 
   tar_target(
     p1_fetch_nlcd_all_years, 
@@ -76,6 +60,13 @@ p1_targets_list <- list(
                    nlcd_year = x,
                    file_name = paste0('nlcd_', x, '.tif'),
                    out_folder = '1_fetch/out/nlcd'))
-    })
+    }),
+  
+  # use DRB boundary to get flowlines from NHD
+  # NHD has streamorder for mapping stream width to later on
+  tar_target(
+    p1_drb_flines,
+    get_nhdplus(AOI = p1_drb_boundary, realization = 'flowline')
+  )
 
 )
