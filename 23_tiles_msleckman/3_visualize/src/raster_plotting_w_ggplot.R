0cda397d6f322efbf6ebb35f7a00509b15c8fc54
collnell <cnell@usgs.gov>
2022-04-30 11:29:34 -0500

    use area data to make a sankey

--- 23_tiles_msleckman/3_visualize/src/raster_plotting_w_ggplot.R
+++ 23_tiles_msleckman/3_visualize/src/raster_plotting_w_ggplot.R
@@ -136,31 +136,23 @@ raster_ploting_w_ggplot <- function(raster_in, reach_shp,
   
 }
 
-# geom_line(size = 3, alpha = 0.7) +
-# geom_point(size = 2, shape = 21, fill = "white", stroke = 1) +
-# theme_classic(base_size = 16)+
-# scale_y_continuous(
-#   labels = scales::label_percent(accuracy = 1),
-#   expand = c(0,0)
-# )+
-# scale_x_continuous(
-#   expand = c(0,0)
-# ) +
-# labs(x="", y="") +
-# theme(
-#   text = element_text(family = font_legend)
-#   #legend.position = 'none',
-#   #plot.background = element_blank(),
-#   #panel.background = element_blank(),
-# )+
-# scale_color_manual(
-#   values = legend_df$color,
-#   labels = legend_df$Reclassify_description,
-#   "Land cover"
-# )+
-# scale_fill_manual(
-#   values = legend_df$color,
-#   labels = legend_df$Reclassify_description,
-#   "Land cover"
-# 
-# )
+plot_sankey <- function(){
+  ggplot(p2_df, aes(
+    x = x,
+    next_x = next_x, 
+    node = node,
+    next_node = next_node,
+    fill = factor(node)
+  ))  +
+    theme_sankey(base_size = 18)+
+    geom_sankey(
+      flow.alpha = 0.6,
+      node.color = "white"
+    )+
+    scale_fill_manual(
+      values = legend_df$color,
+      labels = legend_df$Reclassify_description,
+      "Land cover"
+    )+
+    labs(x='')  
+}
